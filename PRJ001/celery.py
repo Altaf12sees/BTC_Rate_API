12f0b73c37fcfe2999ffa5c241c7518d9ee293f1
Altaf <74228908+Altaf12sees@users.noreply.github.com>
2021-11-25 09:57:10 +0400

    Updated

--- PRJ001/celery.py
+++ PRJ001/celery.py
@@ -5,6 +5,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PRJ001.settings')
 
+
 app = Celery('APP001')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
